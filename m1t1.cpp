a5f398a2ae382a80828a233e5f3c74f7a385d345
Darkwolfattano <nicholas.j.c.thebeau@gmail.com>
2026-02-02 15:58:45 -0500

    started m1t1

---
+++ m1t1.cpp
@@ -0,0 +1,18 @@
+/*
+    CSC 134
+    m1t1- Hello World
+    Nicholas Thebeau
+    2/2/2026
+
+
+
+*/
+
+// Magic words
+#include <iostream>
+using namespace std;
+int main()
+{
+    
+    return 0;
+}
